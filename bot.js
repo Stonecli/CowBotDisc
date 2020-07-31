9ea6593254fc8fafa555a24d32265f65d75cb33e
Stonecli <69026413+Stonecli@users.noreply.github.com>
2020-07-30 23:38:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.NzM4MjQyODYxNDcyMTUzNjEx.XyJEPg.N5QTWk7E42xDkanIf7Z4fblgPGY);//BOT_TOKEN is the Client Secret
